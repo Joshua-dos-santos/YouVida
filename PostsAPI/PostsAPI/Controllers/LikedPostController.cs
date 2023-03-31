ba29d372a5fa9f727c7b3c850065a607fd79bb01
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2023-03-31 13:40:18 +0200

    Added controllers for Post microservice

---
+++ PostsAPI/PostsAPI/Controllers/LikedPostController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PostsAPI.Models;
+
+namespace PostsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LikedPostController : ControllerBase
+    {
+        private readonly ApplicationContext context;
+
+        public LikedPostController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return this.Ok(await this.context.LikedPosts.ToListAsync());
+        }
+
+
+        [HttpGet("{likedPostId:guid}")]
+        public async Task<IActionResult> GetById(Guid likedPostId)
+        {
+            return this.Ok(await this.context.LikedPosts.Where(x => x.LikedPostId == likedPostId).FirstOrDefaultAsync());
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Create(LikedPost likedpost)
+        {
+            await this.context.LikedPosts.AddAsync(likedpost);
+            await this.context.SaveChangesAsync();
+            return this.Ok(likedpost);
+        }
+
+        [HttpPut("")]
+        public async Task<IActionResult> Update(LikedPost likedpost)
+        {
+            this.context.LikedPosts.Update(likedpost);
+            await this.context.SaveChangesAsync();
+            return this.Ok(likedpost);
+        }
+
+        [HttpDelete("{likedpostId:guid}")]
+        public async Task<IActionResult> Delete(Guid likedpostId)
+        {
+            var likedPost = await this.context.LikedPosts.Where(x => x.PostId == likedpostId).FirstAsync();
+            this.context.LikedPosts.Remove(likedPost);
+            return this.Ok(await this.context.SaveChangesAsync());
+        }
+    }
+}
---
+++ PostsAPI/PostsAPI/Controllers/PostController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PostsAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PostsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly ApplicationContext context;
+
+        public PostController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return this.Ok(await this.context.Posts.ToListAsync());
+        }
+
+       
+        [HttpGet("{postId:guid}")]
+        public async Task<IActionResult> GetById(Guid postId)
+        {
+            return this.Ok(await this.context.Posts.Where(x => x.PostId == postId).FirstOrDefaultAsync());
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Create(Post post)
+        {
+            await this.context.Posts.AddAsync(post);
+            await this.context.SaveChangesAsync();
+            return this.Ok(post);
+        }
+
+        [HttpPut("")]
+        public async Task<IActionResult> Update(Post post)
+        {
+            this.context.Posts.Update(post);
+            await this.context.SaveChangesAsync();
+            return this.Ok(post);
+        }
+
+        [HttpDelete("{postId:guid}")]
+        public async Task<IActionResult> Delete(Guid postId)
+        {
+            var post = await this.context.Posts.Where(x => x.PostId == postId).FirstAsync();
+            this.context.Posts.Remove(post);
+            return this.Ok(await this.context.SaveChangesAsync());
+        }
+    }
+}
