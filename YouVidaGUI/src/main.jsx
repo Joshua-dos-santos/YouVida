eb5aef215ca569323a003160bb7d13fc9ebff8cc
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2023-04-06 13:49:44 +0200

    Auth0 authentication

--- YouVidaGUI/src/main.jsx
+++ YouVidaGUI/src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {Auth0Provider} from "@auth0/auth0-react";
+
+const domain = "dev-2h2ymz1zr5cmek6w.eu.auth0.com";
+const clientId = "ZBV5xfwBsGtRuvto1gY0kctzAPXhKVRM";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            authorizationParams={{
+                redirectUri:"http:localhost:8069/"
+            }}
+        >
+            <App/>
+        </Auth0Provider>
+    </React.StrictMode>,
 )
